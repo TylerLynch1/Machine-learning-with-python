afbeee6fe294fd4cb427ccc8ee582bcfd0baa27f
Tyler Lynch <77299122+TylerLynch1@users.noreply.github.com>
2024-07-12 22:10:53 -0400

    Update DecisionTree.py

--- Code/DecisionTree.py
+++ Code/DecisionTree.py
@@ -1,3 +1,7 @@
+import time
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, fbeta_score
+
 # Record the start time for measuring training duration
 start_time = time.time()
 
