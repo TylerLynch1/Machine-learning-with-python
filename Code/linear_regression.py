3bf00f8983ca630e32f1d298f40341d7a7279a8a
Tyler Lynch <77299122+TylerLynch1@users.noreply.github.com>
2024-07-12 22:35:55 -0400

    Update linear_regression.py

--- Code/linear_regression.py
+++ Code/linear_regression.py
@@ -20,8 +20,10 @@ y = df['Price']
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Creating and training the linear regression model
+# Create model
 model = LinearRegression()
+
+# Train model
 model.fit(X_train, y_train)
 
 # Make predictions
